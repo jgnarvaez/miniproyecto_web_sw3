58dc644527f78fdb9d5928622e4715e5d94349a3
jgnarvaez <jgnarvaez@unicauca.edu.co>
2023-01-18 19:53:47 -0500

    Se adiciona Diagramas y Proyecto en entorno de desarrollo Visual Studio 2022

---
+++ miniproyecto_web/miniproyecto_web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace miniproyecto_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
